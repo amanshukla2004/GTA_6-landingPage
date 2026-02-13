847703a9488aefbbb13f81426e7afb816efceda5
Aman Shukla <as.amanshukla2004@gmail.com>
2026-02-13 22:44:25 +0530

    add FirstVideo component with GSAP animations and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import Hero from "./sections/hero";
-//import Hero2 from "./sections/Hero2";
+
 import NavBar from "./sections/NavBar";
 import ComingSoon from "./sections/ComingSoon";
+import FirstVideo from "./sections/FirstVideo";
+
 
 // gsap plugins are not automatically active
 // you can register them using one single line of code
@@ -15,16 +17,14 @@ const App = () => {
     <main>
       <NavBar />
       <Hero />
-      {/* <ComingSoon /> */}
+
+
+      <FirstVideo />
+       
     </main>
   );
 };
 
 export default App;
 
-{
-  /* <nav>
-        <img src="/images/nav-logo.svg" className="scale-90" />
-        <img src="/images/menu.svg" className="w-10" />
-      </nav> */
-}
+
