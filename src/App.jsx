192231be9e4f3454159c299d76effde8eb277766
Aman Shukla <as.amanshukla2004@gmail.com>
2026-02-14 11:30:18 +0530

    fix: correct import case for Hero component and remove unused Hero2 component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
-import Hero from "./sections/hero";
+import Hero from "./sections/Hero";
 import NavBar from "./sections/NavBar";
 import FirstVideo from "./sections/FirstVideo";
 import Jason from "./sections/Jason";
