79f07233b7a98934d9e86da30535b32a398a068f
Aman Shukla <as.amanshukla2004@gmail.com>
2026-02-14 10:57:45 +0530

    add Final component with GSAP animations and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Jason from "./sections/Jason";
 import SecondVideo from "./sections/SecondVideo";
 import Lucia from "./sections/Lucia";
 import PostCard from "./sections/PostCard";
+import Final from "./sections/Final";
 
 // gsap plugins are not automatically active
 // you can register them using one single line of code
@@ -27,6 +28,7 @@ const App = () => {
       <SecondVideo />
       <Lucia />
       <PostCard />
+      <Final />
     </main>
   );
 };
